fc192a1c0d520182961873ee98eed2db3790160c
Rizky Faza <rizkyfaza61@gmail.com>
2021-11-09 23:33:39 +0700

    Added new function to form - Soal 2

--- Soal 2/function.js
+++ Soal 2/function.js
@@ -1,8 +1,8 @@
 function check(form) { /* Check the value of the Username and Password*/
     if(form.login.value == "admin" && form.pass.value == "admin12345"){
-        window.open(alert("Login Successful."))
+        window.open(alert("Login Successful."));
     }
     else{
-        alert("Your username or password are not permitted.")
+        alert("Your username or password are not permitted.");
     }
 }
