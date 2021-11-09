761fb1ca1a60642e9326e2317c24e10b04060981
Rizky Faza <rizkyfaza61@gmail.com>
2021-11-09 12:45:02 +0700

    Added function and login form - Soal 2

---
+++ Soal 2/function.js
@@ -0,0 +1,8 @@
+function check(form) { /* Check the value of the Username and Password*/
+    if(form.login.value == "admin" && form.pass.value == "admin12345"){
+        window.open(alert("Login Successful."))
+    }
+    else{
+        alert("Your username or password are not permitted.")
+    }
+}
